25de9f4f6d570929182ee70521c36020ebaa653a
danieltikamori <tikamori.itiro@gmail.com>
2024-09-29 21:39:19 +0900

    Add CategoryController

---
+++ src/main/java/me/amlume/shop/amlume_shop/model/Category.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlume.shop.amlume_shop.model;
+
+public class Category {
+
+    private Long category_id;
+    private String categoryName;
+
+    public Category(Long category_id, String categoryName) {
+        this.category_id = category_id;
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
+}
