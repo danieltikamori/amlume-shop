72507486ec812b211fb60890b5cfec82332c3859
danieltikamori <tikamori.itiro@gmail.com>
2024-09-29 21:39:00 +0900

    Initial commit

---
+++ src/test/java/me/amlume/shop/amlume_shop/AmlumeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.amlume.shop.amlume_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmlumeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
