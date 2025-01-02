605dd2d2afbbbbc8498c1c93eb134e68b0e5a314
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-01-02 21:44:48 +0900

    Add sortBy and sortDir for pagination
    Include AppConstants to improve maintainability

--- src/main/java/me/amlu/shop/amlume_shop/config/AppConstants.java
+++ src/main/java/me/amlu/shop/amlume_shop/config/AppConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Daniel Itiro Tikamori. All rights reserved.
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
  *
  * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
  *
@@ -8,19 +8,12 @@
  * Please contact the copyright holder at fuiwzchps@mozmail.com for any inquiries or requests for authorization to use the software.
  */
 
-package me.amlu.shop.amlume_shop.service;
+package me.amlu.shop.amlume_shop.config;
 
-import me.amlu.shop.amlume_shop.payload.CategoryDTO;
-import me.amlu.shop.amlume_shop.payload.CategoryResponse;
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+public static final String PAGE_SIZE = "50";
 
-public interface CategoryService {
-
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
-
-    CategoryDTO createCategory(CategoryDTO categoryDTO);
-
-
-    CategoryDTO deleteCategory(Long category_id);
-
-    CategoryDTO updateCategory(Long categoryId, CategoryDTO category);
+public static final String SORT_CATEGORIES_BY = "category_id";
+public static final String SORT_DIR = "asc";
 }
