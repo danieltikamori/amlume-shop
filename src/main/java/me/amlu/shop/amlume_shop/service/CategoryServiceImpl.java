a0b9c5365e092884980c0f473ca501f9ef8d6d72
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-01-02 20:51:27 +0900

    Add initial Pagination implementation

--- src/main/java/me/amlu/shop/amlume_shop/service/CategoryServiceImpl.java
+++ src/main/java/me/amlu/shop/amlume_shop/service/CategoryServiceImpl.java
@@ -18,6 +18,9 @@ import me.amlu.shop.amlume_shop.payload.CategoryDTO;
 import me.amlu.shop.amlume_shop.payload.CategoryResponse;
 import me.amlu.shop.amlume_shop.repositories.CategoryRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,8 +43,11 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public CategoryResponse getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+        List<Category> categories = categoryPage.getContent();
         if (categories.isEmpty())
             throw new NotFoundException("No category found");
 
