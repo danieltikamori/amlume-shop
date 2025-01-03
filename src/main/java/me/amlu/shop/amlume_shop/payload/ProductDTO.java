c56dab06b32f9ba72081e563a9f92e9c8cdaa5c1
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-01-04 04:40:58 +0900

    Add ProductDTOs

---
+++ src/main/java/me/amlu/shop/amlume_shop/payload/ProductDTO.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at fuiwzchps@mozmail.com for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long product_id;
+    private String productName;
+    private String productImage;
+    private Integer  productQuantity;
+    private BigInteger productPrice;
+    private BigInteger productDiscount;
+    private BigInteger productSpecialPrice;
+
+
+}
