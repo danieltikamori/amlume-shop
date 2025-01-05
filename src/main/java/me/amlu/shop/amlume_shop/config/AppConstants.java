f2d98c0a73dde45e3238dfeb2067241f35f2e96a
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-01-05 21:57:16 +0900

    Add constants for pagination and sorting
    Use camelCase convention or sorting may not work properly.
    Ex: using product_id in request, may be interpreted as product.
    Use productId if got errors.

--- src/main/java/me/amlu/shop/amlume_shop/config/AppConstants.java
+++ src/main/java/me/amlu/shop/amlume_shop/config/AppConstants.java
@@ -12,8 +12,9 @@ package me.amlu.shop.amlume_shop.config;
 
 public class AppConstants {
     public static final String PAGE_NUMBER = "0";
-public static final String PAGE_SIZE = "50";
+    public static final String PAGE_SIZE = "50";
+    public static final String SORT_DIR = "asc";
 
-public static final String SORT_CATEGORIES_BY = "category_id";
-public static final String SORT_DIR = "asc";
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_PRODUCTS_BY = "productId";
 }
