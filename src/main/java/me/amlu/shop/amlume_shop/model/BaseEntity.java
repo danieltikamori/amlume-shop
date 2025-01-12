231454ff82e71d04275977043cdd30e0189f080a
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-01-12 12:26:48 +0900

    Add BaseEntity as SuperClass
    It would avoid repetition and make maintainability easier.

---
+++ src/main/java/me/amlu/shop/amlume_shop/model/BaseEntity.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.SoftDelete;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@EntityListeners(AuditingEntityListener.class)
+@SoftDelete
+@FilterDef(name = "deletedFilter", defaultCondition = "deleted_at IS NULL")
+@FilterDef(name = "adminFilter", defaultCondition = "1=1")
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Version
+    private int version;
+
+    @Column(name = "idempotency_key", unique = true,  nullable = false)
+    private String idempotencyKey;
+
+
+    @CreatedDate
+    @NotBlank
+    @Column(name = "created_at", nullable = false, updatable = false,  columnDefinition = "DATETIME ZONE='UTC'")
+    private Instant createdAt;
+
+    @CreatedBy
+    @NotBlank
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false, updatable = false)
+    private User createdByUser;
+
+    @LastModifiedDate
+    @NotBlank
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME ZONE='UTC'")
+    private Instant updatedAt;
+
+    @NotBlank
+    @ManyToOne
+    @JoinColumn(name = "updated_by", nullable = false)
+    private User updatedByUser;
+
+    @SoftDelete
+//    @SensitiveData(rolesAllowed = {"ADMIN", "ROOT"})
+    @Column(nullable = true, name = "deleted_at", columnDefinition = "DATETIME ZONE='UTC'")
+    private Instant deletedAt;
+
+//    @SensitiveData(rolesAllowed = {"ADMIN", "ROOT"})
+    @ManyToOne
+    @JoinColumn(nullable = true, name = "deleted_by")
+    private User deletedByUser;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = Instant.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+    @PreRemove
+    public void preRemove() {
+        this.deletedAt = Instant.now();
+        this.deletedByUser = this.updatedByUser; // getAuthenticatedUser();
+    }
+}
