fb2ef9bfa4dd46c2a2d49759674335a017735d4c
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-02-12 06:33:11 +0900

    Add App roles enum

---
+++ src/main/java/me/amlu/shop/amlume_shop/model/AppRole.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.model;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_CUSTOMER,
+    ROLE_SELLER,
+    ROLE_SELLER_MANAGER,
+    ROLE_SELLER_STAFF,
+    ROLE_SELLER_DELIVERY_PERSON,
+    ROLE_ROOT,
+    ROLE_ADMIN,
+    ROLE_SUPER_ADMIN,
+    ROLE_MANAGER,
+    ROLE_MODERATOR
+}
