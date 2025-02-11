b1f611a1d69090caa0fc05a00edfe45b2f9dceea
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-02-12 06:36:11 +0900

    Add User repository

---
+++ src/main/java/me/amlu/shop/amlume_shop/repositories/UserRepository.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.repositories;
+
+import me.amlu.shop.amlume_shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+
+    User findByEmail(String email);
+
+     Optional<User> findByUsername(String username);
+
+    User findByRefreshToken(String hashpw);
+}
