3b0c980e8a2961365442fca1927e97ac052ca1aa
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-02-14 06:14:24 +0900

    Add initial ResilienceConfig
    Resilience4j
    - Circuit breaker
    - Time Limiter
    - Retry
    - Rest template

---
+++ src/main/java/me/amlu/shop/amlume_shop/config/ResilienceConfig.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+public class ResilienceConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000); // 5 seconds
+        requestFactory.setReadTimeout(10000); // 10 seconds
+        restTemplate.setRequestFactory(requestFactory);
+        return restTemplate;
+    }
+
+    @Bean
+    public CircuitBreakerRegistry circuitBreakerRegistry() {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofSeconds(20))
+                .permittedNumberOfCallsInHalfOpenState(5)
+                .slidingWindowSize(10)
+                .build();
+
+        return CircuitBreakerRegistry.of(circuitBreakerConfig);
+    }
+
+    @Bean
+    public TimeLimiterRegistry timeLimiterRegistry() {
+        TimeLimiterConfig config = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(1))
+                .build();
+        return TimeLimiterRegistry.of(config);
+    }
+
+    @Bean
+    public RetryRegistry retryRegistry() {
+        RetryConfig retryConfig = RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofSeconds(1))
+                .retryExceptions(RestClientException.class)
+                .build();
+
+        return RetryRegistry.of(retryConfig);
+    }
+}
