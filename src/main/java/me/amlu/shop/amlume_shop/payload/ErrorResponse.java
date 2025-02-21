9c89a78949c058369cf8386dbc5fcd155c95c8ec
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-02-22 08:37:25 +0900

    Add initial ErrorResponse
    Used mainly for authentication

---
+++ src/main/java/me/amlu/shop/amlume_shop/payload/ErrorResponse.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String code;
+    private String message;
+
+    public ErrorResponse(String tooManyDevices, String message, int value) {
+    }
+}
