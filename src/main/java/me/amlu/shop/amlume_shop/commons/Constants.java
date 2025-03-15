7cf3c8544a445e392b09a105bf1f9549d27140fe
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-03-15 22:08:01 +0900

    Add common Constants for maintainability and performance purposes
    Import to other classes without instantiation.

---
+++ src/main/java/me/amlu/shop/amlume_shop/commons/Constants.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.commons;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Slf4j
+@Component
+@Getter
+public class Constants {
+    // Pagination constants
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "50";
+    public static final String SORT_DIR = "asc";
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_PRODUCTS_BY = "productId";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+
+    // Tolerances
+    // For time-based validation
+    @Value("${clock.skew.tolerance}")
+    public static final Duration CLOCK_SKEW_TOLERANCE = Duration.ofSeconds(10);
+
+    // Authentication and Authorization constants
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    // Http header constants
+    public static final String USER_AGENT = "User-Agent";
+    public static final String ACCEPT_LANGUAGE = "Accept-Language";
+    public static final String TIME_ZONE = "X-Time-Zone";
+    public static final String X_FORWARDED_FOR = "X-Forwarded-For";
+//    public static final String X_FORWARDED_PROTO = "X-Forwarded-Proto";
+//    public static final String X_FORWARDED_PORT = "X-Forwarded-Port";
+//    public static final String X_FORWARDED_HOST = "X-Forwarded-Host";
+//    public static final String X_FORWARDED_PREFIX = "X-Forwarded-Prefix";
+//    public static final String X_FORWARDED_SERVER = "X-Forwarded-Server";
+//    public static final String X_FORWARDED_URI = "X-Forwarded-Uri";
+//    public static final String X_FORWARDED_SCHEME = "X-Forwarded-Scheme";
+//    public static final String X_FORWARDED_METHOD = "X-Forwarded-Method";
+//    public static final String X_FORWARDED_FORWARDED = "X-Forwarded-Forwarded";
+//    public static final String X_FORWARDED_HOSTNAME = "X-Forwarded-Hostname";
+//    public static final String X_FORWARDED_SERVER_NAME = "X-Forwarded-Server-Name";
+//    public static final String X_FORWARDED_SERVER_PORT = "X-Forwarded-Server-Port";
+//    public static final String X_FORWARDED_SERVER_PROTOCOL = "X-Forwarded-Server-Protocol";
+//    public static final String X_FORWARDED_SERVER_SCHEME = "X-Forwarded-Server-Scheme";
+//    public static final String X_FORWARDED_SERVER_URI = "X-Forwarded-Server-Uri";
+
+    // User constants
+    public static final String USER_ID = "userId";
+    public static final String USERNAME = "XXXXXXXX";
+    public static final String PASSWORD = "XXXXXXXX";
+    public static final String EMAIL = "XXXXXXXX";
+    public static final String USER_NOT_FOUND = "User not found";
+
+    // Security constants
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final int MAX_FAILED_ATTEMPTS = 5;
+    public static final long LOCK_DURATION_MILLIS = 1000 * 60 * (long) 60;
+    public static final String HASH_ALGORITHM = "SHA-256";
+    public static final int INITIAL_MAP_CAPACITY = 16;
+    public static final String[] IP_HEADERS = {
+            X_FORWARDED_FOR,
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_CLIENT_IP",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    private static final String LINE_SEPARATOR = "\n";
+
+    private Constants() {
+    } // Private constructor to prevent instantiation
+
+}
