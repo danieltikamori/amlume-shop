67c648f01e2a4627ab5a12231de35cad04744ff4
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-03-16 22:04:57 +0900

    Add Email sending service

---
+++ src/main/java/me/amlu/shop/amlume_shop/exceptions/EmailSendingFailedException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.exceptions;
+
+import java.io.Serial;
+
+public class EmailSendingFailedException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public EmailSendingFailedException(Exception e) {
+        super("Failed to send email", e);
+    }
+
+    public EmailSendingFailedException(String message) {
+        super(message);
+    }
+
+    public EmailSendingFailedException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public EmailSendingFailedException() {
+        super("Failed to send email");
+    }
+
+    public EmailSendingFailedException(Exception e, String message) {
+        super(message, e);
+    }
+
+    public EmailSendingFailedException(String message, Exception e, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, e, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/me/amlu/shop/amlume_shop/service/EmailServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.service;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import me.amlu.shop.amlume_shop.enums.ResponseMessage;
+import me.amlu.shop.amlume_shop.exceptions.EmailSendingFailedException;
+import me.amlu.shop.amlume_shop.payload.EmailDto;
+import me.amlu.shop.amlume_shop.payload.Response;
+import me.amlu.shop.amlume_shop.security.service.EmailService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailServiceImpl implements EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Override
+    public Object sendMail(EmailDto emailDto) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setFrom(fromEmail);
+            mimeMessageHelper.setTo(emailDto.getToList());
+            mimeMessageHelper.setSubject(emailDto.getSubject());
+            mimeMessageHelper.setText(emailDto.getBody());
+
+            javaMailSender.send(mimeMessage);
+
+            log.info("Message Sent Successfully to: {}", emailDto.getToList());
+            return new Response("Email Sent Successfully", emailDto.getToList(), ResponseMessage.SUCCESS);
+        } catch (Exception e) {
+            log.error("sendEmail() | Error : {}", e.getMessage());
+            throw new EmailSendingFailedException(e);
+        }
+    }
+
+}
