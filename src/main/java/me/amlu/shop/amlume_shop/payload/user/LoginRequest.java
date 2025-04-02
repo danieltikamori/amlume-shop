65500f82c07410c71b3db84e6ff03e357409deb5
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-03 06:35:44 +0900

    Add LoginRequest and LoginResponse classes for user authentication handling

---
+++ src/main/java/me/amlu/shop/amlume_shop/payload/user/LoginRequest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.payload.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    private String mfaCode;
+
+    @NotBlank
+    private String captchaResponse;
+
+    // Device fingerprint fields
+    private String userAgent; // Add User-Agent field
+    private String screenWidth; // Add screen width
+    private String screenHeight; // Add screen height
+
+    private boolean rememberMe;
+
+
+
+}
+
