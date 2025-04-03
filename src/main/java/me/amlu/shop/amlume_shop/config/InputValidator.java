3d7138e29fd25621ce04a8fed7baa4a0109ac4ea
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-03 22:19:52 +0900

    Refactor InputValidator: update Product import to use product_management package

--- src/main/java/me/amlu/shop/amlume_shop/config/InputValidator.java
+++ src/main/java/me/amlu/shop/amlume_shop/config/InputValidator.java
@@ -13,7 +13,7 @@ package me.amlu.shop.amlume_shop.config;
 import lombok.extern.slf4j.Slf4j;
 import me.amlu.shop.amlume_shop.category_management.Category;
 import me.amlu.shop.amlume_shop.model.Order;
-import me.amlu.shop.amlume_shop.model.Product;
+import me.amlu.shop.amlume_shop.product_management.Product;
 import org.springframework.stereotype.Component;
 
 @Component
