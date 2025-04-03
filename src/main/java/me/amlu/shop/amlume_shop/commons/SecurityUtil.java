33baa770f96cb9c09a2eef318102366e17a3ae68
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-03 20:16:52 +0900

    Fix import statement for User class in SecurityUtil

--- src/main/java/me/amlu/shop/amlume_shop/commons/SecurityUtil.java
+++ src/main/java/me/amlu/shop/amlume_shop/commons/SecurityUtil.java
@@ -10,7 +10,7 @@
 
 package me.amlu.shop.amlume_shop.commons;
 
-import me.amlu.shop.amlume_shop.model.User;
+import me.amlu.shop.amlume_shop.user_management.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
