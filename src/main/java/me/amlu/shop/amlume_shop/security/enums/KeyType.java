092e6663259fa5b07f5a877a148d265cd11d71f8
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-04 21:52:42 +0900

    Add KeyType: implement enum for different key types used in security

---
+++ src/main/java/me/amlu/shop/amlume_shop/security/enums/KeyType.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.security.enums;
+
+// Secret/Private key types
+public enum KeyType {
+    PUBLIC_KEY,
+    PRIVATE_KEY,
+    SECRET_KEY,
+    LOCAL,
+    PUBLIC,
+
+}
