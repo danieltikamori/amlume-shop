cc3d58be2086fe97256c12ef5072c2cc35489909
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-04 20:58:28 +0900

    Add TokenResponse: implement class for managing token response data

---
+++ src/main/java/me/amlu/shop/amlume_shop/payload/TokenResponse.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.shop.amlume_shop.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+}
+
+
