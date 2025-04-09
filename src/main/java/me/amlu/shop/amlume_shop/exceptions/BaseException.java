371d37afd027c0354aeb10918f110cd4d76b95ca
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-10 05:59:29 +0900

    Add constructors to BaseException: implement additional constructors for improved exception handling

--- src/main/java/me/amlu/shop/amlume_shop/exceptions/BaseException.java
+++ src/main/java/me/amlu/shop/amlume_shop/exceptions/BaseException.java
@@ -24,4 +24,12 @@ public class BaseException extends RuntimeException {
     public BaseException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+    public BaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);}
+
 }
