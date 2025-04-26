8a6bd4a5282a6bb0a67a4e30d41953e1d2f742e3
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-26 21:08:30 +0900

    feat(resilience): Refactor ResilienceConfig to use configuration properties for better maintainability

--- src/main/java/me/amlu/shop/amlume_shop/config/ResilienceConfig.java
+++ src/main/java/me/amlu/shop/amlume_shop/config/ResilienceConfig.java
@@ -19,8 +19,8 @@ import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.convert.DurationUnit;
+import me.amlu.shop.amlume_shop.resilience.properties.*;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -28,8 +28,6 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.SocketTimeoutException;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -39,69 +37,33 @@ import java.util.concurrent.ScheduledExecutorService;
  * Provides configurations for Circuit Breaker, Time Limiter, Retry, Bulkhead, and other resilience patterns.
  */
 @Configuration
+@EnableConfigurationProperties({Resilience4jBulkheadProperties.class,
+        Resilience4jCircuitBreakerProperties.class,
+        Resilience4jExecutorProperties.class,
+        Resilience4jExponentialBackoffProperties.class,
+        Resilience4jRetryProperties.class,
+        Resilience4jTimeLimiterProperties.class,
+        RestTemplateProperties.class
+})
 public class ResilienceConfig {
 
-@Value("${resilience4j.rest-template.connect-timeout:5000}")
-    public int CONNECT_TIMEOUT;
-@Value("${resilience4j.rest-template.read-timeout:10000}")
-    public int READ_TIMEOUT;
-    
-    // --- Bulkhead constants ---
-    @Value("${resilience4j.bulkhead.instances.default.max-concurrent-calls:100}")
-    public int MAX_CONCURRENT_CALLS;
-    @Value("${resilience4j.bulkhead.max-wait-duration:300}")
-    public Duration MAX_WAIT_DURATION;
+    private final RestTemplateProperties restTemplateProperties;
+    private final Resilience4jBulkheadProperties bulkheadProperties;
+    private final Resilience4jCircuitBreakerProperties circuitBreakerProperties;
+    private final Resilience4jExecutorProperties executorProperties;
+    private final Resilience4jExponentialBackoffProperties exponentialBackoffProperties;
+    private final Resilience4jRetryProperties retryProperties;
+    private final Resilience4jTimeLimiterProperties timeLimiterProperties;
 
-    // --- Circuit breaker constants ---
-    @Value("${resilience4j.circuitbreaker.instances.default.failure-rate-threshold:50}")
-    public float FAILURE_RATE_THRESHOLD;
-
-    @Value("${resilience4j.circuitbreaker.instances.default.wait-duration-in-open-state:20}")
-    public Duration WAIT_DURATION_IN_OPEN_STATE;
-
-    @Value("${resilience4j.circuitbreaker.instances.default.permitted-number-of-calls-in-half-open-state:5}")
-    public int PERMITTED_NUMBER_OF_CALLS_IN_HALF_OPEN_STATE;
-
-    @Value("${resilience4j.circuitbreaker.instances.default.sliding-window-size:100}")
-    public int DEFAULT_CIRCUIT_BREAKER_SLIDING_WINDOW_SIZE = MAX_CONCURRENT_CALLS;
-
-    // --- Rate limiter constants ---
-
-    // --- Retry constants ---
-    @Value("${resilience4j.retry.instances.default.max-attempts:3}")
-    public int MAX_RETRY_ATTEMPTS;
-
-    @Value("${resilience4j.retry.instances.default.max-requests-per-minute:100L}")
-    public Long MAX_REQUESTS_PER_MINUTE;
-
-    @Value("${resilience4j.retry.instances.default.retry-interval:1000L}")
-    public long RETRY_INTERVAL;
-
-    @Value("${resilience4j.retry.instances.default.retry-wait-duration:200}")
-    @DurationUnit(ChronoUnit.MILLIS) // Specify unit if property is just a number (e.g., 500)
-    public Duration RETRY_WAIT_DURATION;
-
-    // --- Exponential backoff constants ---
-    @Value("${resilience4j.exponential-backoff.instances.default.initial-interval-millis:200}")
-    private long INITIAL_INTERVAL_MILLIS;
-
-    @Value("${resilience4j.exponential-backoff.instances.default.eb-multiplier:1.5}")
-    private double EB_MULTIPLIER;
-
-    @Value("${resilience4j.exponential-backoff.instances.default.randomization-factor:0.36}")
-    private double RANDOMIZATION_FACTOR;
-
-    @Value("${resilience4j.exponential-backoff.instances.default.max-interval-millis:86400000L}")
-    private long MAX_INTERVAL_MILLIS;
-
-    // --- Time limiter constants ---
-    @Value("${resilience4j.time-limiter.instances.default.timeout-duration:1}")
-    @DurationUnit(ChronoUnit.SECONDS) // Specify unit if property is just a number (e.g., 500)
-    public Duration TIMEOUT_DURATION;
-
-    // --- Executor constants ---
-    @Value("${resilience4j.executor.instances.default.core-pool-size:4}")
-    public int CORE_POOL_SIZE;
+    public ResilienceConfig(RestTemplateProperties restTemplateProperties, Resilience4jBulkheadProperties bulkheadProperties, Resilience4jCircuitBreakerProperties circuitBreakerProperties, Resilience4jExecutorProperties executorProperties, Resilience4jExponentialBackoffProperties exponentialBackoffProperties, Resilience4jRetryProperties retryProperties, Resilience4jTimeLimiterProperties timeLimiterProperties) {
+        this.restTemplateProperties = restTemplateProperties;
+        this.bulkheadProperties = bulkheadProperties;
+        this.circuitBreakerProperties = circuitBreakerProperties;
+        this.executorProperties = executorProperties;
+        this.exponentialBackoffProperties = exponentialBackoffProperties;
+        this.retryProperties = retryProperties;
+        this.timeLimiterProperties = timeLimiterProperties;
+    }
 
     /**
      * Configures a RestTemplate bean with custom connection and read timeouts.
@@ -112,8 +74,8 @@ public class ResilienceConfig {
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(CONNECT_TIMEOUT); // 5 seconds
-        requestFactory.setReadTimeout(READ_TIMEOUT); // 10 seconds
+        requestFactory.setConnectTimeout(restTemplateProperties.getConnectTimeout()); // 5 seconds
+        requestFactory.setReadTimeout(restTemplateProperties.getReadTimeout()); // 10 seconds
         restTemplate.setRequestFactory(requestFactory);
         return restTemplate;
     }
@@ -126,8 +88,8 @@ public class ResilienceConfig {
     @Bean
     public BulkheadRegistry bulkheadRegistry() {
         BulkheadConfig config = BulkheadConfig.custom()
-                .maxConcurrentCalls(MAX_CONCURRENT_CALLS) // Allow up to 100 concurrent calls
-                .maxWaitDuration(MAX_WAIT_DURATION) // Wait up to 300 ms for a free slot
+                .maxConcurrentCalls(bulkheadProperties.getMaxConcurrentCalls()) // Allow up to 100 concurrent calls
+                .maxWaitDuration(bulkheadProperties.getMaxWaitDuration()) // Wait up to 300 ms for a free slot
                 .build();
         return BulkheadRegistry.of(config);
     }
@@ -139,11 +101,14 @@ public class ResilienceConfig {
      */
     @Bean
     public CircuitBreakerRegistry circuitBreakerRegistry() {
+
+        // Handle sliding window size default logic here
+        int slidingWindowSize = bulkheadProperties.getMaxConcurrentCalls(); // Default to maxConcurrentCalls if not set
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-                .failureRateThreshold(FAILURE_RATE_THRESHOLD) // The Circuit opens if 50% of calls fail.
-                .waitDurationInOpenState(WAIT_DURATION_IN_OPEN_STATE) // Wait 20 seconds before transitioning to half-open state.
-                .permittedNumberOfCallsInHalfOpenState(PERMITTED_NUMBER_OF_CALLS_IN_HALF_OPEN_STATE) // Allow 5 calls in half-open state.
-                .slidingWindowSize(DEFAULT_CIRCUIT_BREAKER_SLIDING_WINDOW_SIZE) // Use a sliding window of 100 calls for metrics.
+                .failureRateThreshold(circuitBreakerProperties.getFailureRateThreshold()) // The Circuit opens if 50% of calls fail.
+                .waitDurationInOpenState(circuitBreakerProperties.getWaitDurationInOpenState()) // Wait 20 seconds before transitioning to half-open state.
+                .permittedNumberOfCallsInHalfOpenState(circuitBreakerProperties.getPermittedNumberOfCallsInHalfOpenState()) // Allow 5 calls in half-open state.
+                .slidingWindowSize(slidingWindowSize) // Use the calculated/retrieved size for metrics.
                 .build();
 
         return CircuitBreakerRegistry.of(circuitBreakerConfig);
@@ -156,9 +121,9 @@ public class ResilienceConfig {
      */
     @Bean
     public RetryRegistry retryRegistry() {
-        RetryConfig retryConfig = RetryConfig.custom()
-                .maxAttempts(PERMITTED_NUMBER_OF_CALLS_IN_HALF_OPEN_STATE) // Retry up to 5 times
-                .waitDuration(RETRY_WAIT_DURATION) // Wait initially 200 ms between retries
+        RetryConfig.Builder<Object> builder = RetryConfig.custom()
+                .maxAttempts(retryProperties.getMaxRetryAttempts()) // Retry up to 5 times
+                .waitDuration(retryProperties.getRetryWaitDuration()) // Wait initially 200 ms between retries
                 // If the client is a real person,
                 // evaluate exponential backoff is really necessary (critical use cases where reliability is a must).
                 // For non-human clients, like application, exponential backoff may make sense.
@@ -170,10 +135,21 @@ public class ResilienceConfig {
                 // Simplify and avoid potential runtime errors by just handling specific exceptions
                 .retryOnException(t -> t instanceof RestClientException || (t instanceof SocketTimeoutException /*&& t.getMessage() != null && t.getMessage().toLowerCase().contains("timeout")*/)) // Retry on specific exceptions and conditions
                 // Optional: Customize retry conditions further
-                 .retryOnResult(Objects::isNull) // Retry on a specific result
-                // .retryOnPredicate(Predicate.not(response -> response.getStatusCode().is2xxSuccessful())) // Retry on non-2xx status codes
-                .build();
-        return RetryRegistry.of(retryConfig);
+                .retryOnResult(Objects::isNull); // Retry on a specific result
+        // .retryOnPredicate(Predicate.not(response -> response.getStatusCode().is2xxSuccessful())) // Retry on non-2xx status codes
+
+        // Conditionally add exponential backoff based on configuration if desired
+        // Boolean flag in RetryDefaultProperties: useExponentialBackoff=true/false
+        if (retryProperties.isUseExponentialBackoff()) {
+            builder.intervalFunction(IntervalFunction.ofExponentialRandomBackoff(
+                    exponentialBackoffProperties.getInitialIntervalMillis(),
+                    exponentialBackoffProperties.getEbMultiplier(),
+                    exponentialBackoffProperties.getRandomizationFactor(),
+                    exponentialBackoffProperties.getMaxIntervalMillis()
+            ));
+        }
+
+        return RetryRegistry.of(builder.build());
     }
 
     /**
@@ -184,7 +160,7 @@ public class ResilienceConfig {
     @Bean
     public TimeLimiterRegistry timeLimiterRegistry() {
         TimeLimiterConfig config = TimeLimiterConfig.custom()
-                .timeoutDuration(TIMEOUT_DURATION) // Set timeout duration to 1 second
+                .timeoutDuration(timeLimiterProperties.getTimeoutDuration()) // Set timeout duration to 1 second
                 .build();
         return TimeLimiterRegistry.of(config);
     }
@@ -192,14 +168,13 @@ public class ResilienceConfig {
     /**
      * Configures a ScheduledExecutorService bean for managing timeouts on captcha validation calls.
      *
-     * @return a ScheduledExecutorService instance with a thread pool size of 4.
+     * @return a ScheduledExecutorService instance with a thread pool size of 4?.
      */
     @Bean(name = "captchaTimeLimiterExecutor") // Use the specific name required by the @Qualifier
     public ScheduledExecutorService captchaTimeLimiterExecutor() {
         // Choose an appropriate pool size.
         // Start with a small number and monitor.
         // This pool is specifically for managing timeouts on captcha validation calls.
-        return Executors.newScheduledThreadPool(CORE_POOL_SIZE); // Example: pool size of 4
+        return Executors.newScheduledThreadPool(executorProperties.getCorePoolSize()); // Pool size
     }
 }
-
