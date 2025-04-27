6e7c5709c8292559478fe9e70ec4238ea2ceacee
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2025-04-28 05:27:35 +0900

    fix(captcha): Update RECAPTCHA_SECRET_KEY variable to use correct casing

--- src/main/java/me/amlu/shop/amlume_shop/security/service/CaptchaService.java
+++ src/main/java/me/amlu/shop/amlume_shop/security/service/CaptchaService.java
@@ -52,8 +52,8 @@ public class CaptchaService {
     private static final String RECAPTCHA_RETRY = "recaptchaRetry";
     private static final String RECAPTCHA_TIME_LIMITER = "recaptchaTimeLimiter";
 
-//    @Value("${RECAPTCHA_SECRET_KEY}")
-    @Value("${recaptcha.secret}")
+//    @Value("${recaptcha.secret}")
+    @Value("${RECAPTCHA_SECRET_KEY}")
     private String recaptchaSecret;
 
     private final RestTemplate restTemplate;
