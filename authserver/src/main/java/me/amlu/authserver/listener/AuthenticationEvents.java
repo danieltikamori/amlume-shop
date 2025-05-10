5653529c935023cc606646b4aa1a69821519e6bd
danieltikamori <tikamori.itiro@gmail.com>
2025-05-11 04:52:09 +0900

    feat(auth): create new AuthenticationEvents class for handling authentication events

---
+++ authserver/src/main/java/me/amlu/authserver/listener/AuthenticationEvents.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.authserver.listener;
+
+import me.amlu.authserver.service.UserServiceInterface;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AuthenticationEvents {
+
+    private final UserServiceInterface userService;
+
+    public AuthenticationEvents(UserServiceInterface userService) {
+        this.userService = userService;
+    }
+
+    @EventListener
+    public void handleAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) {
+        String username = event.getAuthentication().getName();
+        if (username != null) {
+            userService.handleFailedLogin(username);
+        }
+    }
+
+    @EventListener
+    public void handleAuthenticationSuccess(AuthenticationSuccessEvent event) {
+        Object principal = event.getAuthentication().getPrincipal();
+        String username = null;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            username = (String) principal;
+        }
+
+        if (username != null) {
+            userService.handleSuccessfulLogin(username);
+        }
+    }
+}
+    
