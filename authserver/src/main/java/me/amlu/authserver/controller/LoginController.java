186a34738e5e13d97b43622badec7f443af6727e
Daniel Tikamori <tikamori.itiro@gmail.com>
2025-05-24 12:25:21 +0900

    feat: login page using jte

---
+++ authserver/src/main/java/me/amlu/authserver/controller/LoginController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.authserver.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model, String error, String logout) {
+
+        if (error != null) {
+            model.addAttribute("error", true);
+            model.addAttribute("errorMessage", "Invalid username or password");
+        }
+
+        return "pages/login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "pages/home";
+    }
+}
