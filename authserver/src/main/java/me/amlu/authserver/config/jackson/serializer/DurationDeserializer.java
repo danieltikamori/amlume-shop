7d0d1fa52e91e6b55052046df16bbc24b9f92dcc
Daniel Tikamori <tikamori.itiro@gmail.com>
2025-06-08 12:40:02 +0900

    feat: Custom deserializer for java.time.Duration that converts milliseconds to Duration.
    This is used to ensure proper deserialization of Duration objects in WebAuthn session data.

---
+++ authserver/src/main/java/me/amlu/authserver/config/jackson/serializer/DurationDeserializer.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Daniel Itiro Tikamori. All rights reserved.
+ *
+ * This software is proprietary, not intended for public distribution, open source, or commercial use. All rights are reserved. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, electronic or mechanical, including photocopying, recording, or by any information storage or retrieval system, without the prior written permission of the copyright holder.
+ *
+ * Permission to use, copy, modify, and distribute this software is strictly prohibited without prior written authorization from the copyright holder.
+ *
+ * Please contact the copyright holder at echo ZnVpd3pjaHBzQG1vem1haWwuY29t | base64 -d && echo for any inquiries or requests for authorization to use the software.
+ */
+
+package me.amlu.authserver.config.jackson.serializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.Duration;
+
+/**
+ * Custom deserializer for java.time.Duration that converts milliseconds to Duration.
+ * This is used to ensure proper deserialization of Duration objects in WebAuthn session data.
+ */
+public class DurationDeserializer extends JsonDeserializer<Duration> {
+    @Override
+    public Duration deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        long millis = p.getValueAsLong();
+        return Duration.ofMillis(millis);
+    }
+}
